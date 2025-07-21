ff2892c60264667767d0b109b152524a50f27b10
Daniel Azevedo <daniazevedo77@posteo.net>
2025-07-21 23:49:45 +0100

    Qtile .Xresources

--- qtile/skel/.config/qtile/autostart.sh
+++ qtile/skel/.config/qtile/autostart.sh
@@ -6,7 +6,8 @@ dunst &
 udiskie -a &
 xcompmgr -c -f -n &
 xautolock -time 5 -locker slock &
-redshift -l 41.6:-8.62 -t 5700:3500 -b 0.8 &
+#redshift -l 41.6:-8.62 -t 5700:3500 -b 0.8 &
 synclient TapButton1=1 &
 synclient TapButton2=3 &
 synclient TapButton3=2 &
+xrdb merge ~/.Xresources &
