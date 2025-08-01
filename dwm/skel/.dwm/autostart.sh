61deb0dc5b3321770d944c67dcba79e6de826ade
Daniel Azevedo <daniazevedo77@posteo.net>
2025-08-01 23:16:30 +0100

    bspwm and dwm: autostart scripts cleaned

--- dwm/skel/.dwm/autostart.sh
+++ dwm/skel/.dwm/autostart.sh
@@ -1,22 +1,30 @@
+#!/bin/bash
+
 # notifications
 exec dunst &
+
 # transparency
 exec xcompmgr -c -f -n &
+
 # nightshift for Oporto, Portugal location
 exec redshift -l 41.6:-8.62 & 
+
 # keybind mapper
 exec sxhkd -c ~/.config/sxhkd/sxhkdrc &
+
 # automount disks
 exec udiskie -a &
+
 # wallpaper
 exec feh --bg-fill ~/Wallpaper/dwm_d77.png &
-#exec xrdb merge ~/.Xresources &
+exec xrdb merge ~/.Xresources &
+
 # screenlocker
 exec xautolock -time 10 -locker slock &
+
 # bar
 exec slstatus &
-# keymap
-#exec setxkbmap de deadtilde &
+
 # touchpad
 exec synclient TapButton1=1 &
 exec synclient TapButton2=3 &
