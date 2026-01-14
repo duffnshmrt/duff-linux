7f73b93143ab825fddfac46975c03c2b38b76ab2
Daniel Azevedo <daniazevedo77@posteo.net>
2026-01-14 22:16:28 +0000

    hyprland: 0.49 -> 0.53

--- extras/d77.aliases.bash
+++ extras/d77.aliases.bash
@@ -20,7 +20,7 @@ function _set_pkg_aliases() {
 		alias krem='sudo vkpurge rm'
 
 		alias isobuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/'
-		alias hyprbuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/ -r https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc'
+		alias hyprbuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/ -r https://raw.githubusercontent.com/sofijacom/hyprland-void/repository-x86_64-glibc'
 		alias cosmicbuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/ -r https://bellawagner.de/repo/x86_64 -b cosmic -- -T d77void'
 	fi
 }
