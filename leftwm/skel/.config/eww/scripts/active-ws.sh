ec03831d6515743ea5e5a9740d7095c917d97316
Daniel Azevedo <daniazevedo77@posteo.net>
2026-03-25 23:04:41 +0000

    leftwm; final d77 theme

--- leftwm/skel/.config/eww/scripts/active-ws.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-leftwm-state -q -t '{{ workspaces | map: "tag" | first }}' 2>/dev/null || echo "1"
--- leftwm/skel/.config/eww/scripts/title.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if command -v xtitle >/dev/null 2>&1; then
-  xtitle | head -n1
-else
-  echo ""
-fi
